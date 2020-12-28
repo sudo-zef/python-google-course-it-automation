a1f1786bdff16a84922e0cd41cc3f065ba31b50f
Zohaib <zohaibhstart@gmail.com>
2020-12-28 22:22:24 +0500

    Add files via upload

---
+++ python-google-course-it-automation/week1/week1.py
@@ -0,0 +1,5 @@
+print("I will be making golden ratio for you guys")
+times = int(input("How many times do you want the golden ratio? "))
+for i in range(times):
+    ratio = (1 + 5 ** 0.5) / 2
+    print(ratio)
---
+++ python-google-course-it-automation/week2/week2.py
@@ -0,0 +1,11 @@
+def greeting(name, surname):
+    print("Welcome " + name + surname)
+
+greeting("Zohaib", " Arsalan")
+
+def greetings():
+    name = input("What is your name? ")
+    surname = input("What is your surname? ")
+    print("Welcome " + name + " " + surname)
+
+greetings()
